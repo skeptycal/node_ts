d8eb77744d4b4736492dda8fadc0c46594b7ca26
skeptycal <skeptycal@gmail.com>
2019-04-28 14:42:52 -0500

    initial commit

---
+++ bokeh.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+""" bokeh.py """
+# @imports
+import json  # requires python >= 2.6
+import sys
+from typing import Dict
+
+# @info
+name = "bokeh"
+json_file = sys.path[0] + "/" + name + ".json"
+
+# @functions
